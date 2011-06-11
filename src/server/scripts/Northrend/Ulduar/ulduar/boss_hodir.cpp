2caa3fdf86373407ce7a8ae6413951afdab50327
QAston <qaston@gmail.com>
2011-06-11 12:42:01 +0200

    Scripts: Fix some crashes occuring due to unsafe use of AuraScript::GetCaster() method. There's "returns unit which casted the aura or NULL if not avalible" written in description of the method, but who cares...

--- src/server/scripts/Northrend/Ulduar/ulduar/boss_hodir.cpp
+++ src/server/scripts/Northrend/Ulduar/ulduar/boss_hodir.cpp
@@ -912,7 +912,7 @@ class spell_biting_cold : public SpellScriptLoader
                 Unit* target = GetTarget();
                 bool found = false;
 
-                if (!caster || !target)
+                if (!caster)
                     return;
 
                 for (TargetList::iterator itr = listOfTargets.begin(); itr != listOfTargets.end(); ++itr)
