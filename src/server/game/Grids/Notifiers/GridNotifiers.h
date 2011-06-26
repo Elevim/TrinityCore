43f43fd1efa6c8abc5c9ce804d89ecb6f8e07191
Shocker <shocker@freakz.ro>
2011-06-26 16:06:23 +0300

    Core/Spells: Don't allow Killing Spree to hit unattackable targets

--- src/server/game/Grids/Notifiers/GridNotifiers.h
+++ src/server/game/Grids/Notifiers/GridNotifiers.h
@@ -802,10 +802,10 @@ namespace Trinity
             float i_range;
     };
 
-    class AnyUnfriendlyVisibleUnitInObjectRangeCheck
+    class AnyUnfriendlyAttackableVisibleUnitInObjectRangeCheck
     {
         public:
-            AnyUnfriendlyVisibleUnitInObjectRangeCheck(WorldObject const* obj, Unit const* funit, float range)
+            AnyUnfriendlyAttackableVisibleUnitInObjectRangeCheck(WorldObject const* obj, Unit const* funit, float range)
                 : i_obj(obj), i_funit(funit), i_range(range) {}
 
             bool operator()(Unit* u)
@@ -813,6 +813,7 @@ namespace Trinity
                 return u->isAlive()
                     && i_obj->IsWithinDistInMap(u, i_range)
                     && !i_funit->IsFriendlyTo(u)
+                    && i_funit->canAttack(u)
                     && u->GetCreatureType() != CREATURE_TYPE_CRITTER
                     && i_funit->canSeeOrDetect(u);
             }
